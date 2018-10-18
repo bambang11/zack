82a374fb25edfb8f85ee3a8fa4b9a5202b39ac8e
blablabla <43849412+dolokia@users.noreply.github.com>
2018-10-18 11:41:31 +0700

    Add files via upload

---
+++ zz.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>ZAKPAY Wgans</title>
+
+  <link rel="stylesheet" type="text/css" media="screen" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" />
+
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+  <link href="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/build/css/bootstrap-datetimepicker.css" rel="stylesheet">
+
+  <script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.min.js"></script>
+
+  <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+  <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment-with-locales.js"></script>
+
+  <script src="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/src/js/bootstrap-datetimepicker.js"></script>
+
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css">
+
+  <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+
+  <script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js"></script>
+
+  <script src='https://www.google.com/recaptcha/api.js'></script>
+
+</head>
+
+<body>
+
+  <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+
+    <div class="container">
+
+      <div class="navbar-header">
+
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+
+          <span class="sr-only">Toggle navigation</span>
+
+          <span class="icon-bar"></span>
+
+          <span class="icon-bar"></span>
+
+          <span class="icon-bar"></span>
+
+        </button>
+
+        <a class="navbar-brand" href="/">ZAKPAY Wgans</a>
+
+        </div>
+
+        <div class="navbar-collapse collapse">
+
+          <ul class="nav navbar-nav">
+
+       </ul>
+
+      </div>
+
+    </div>
+
+  </div>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+ 
+
+
+
+     <div class="col-md-4">
+
+    <div class="panel panel-default">
+
+      <div class="panel-heading"><i class="fa fa-code"></i> ZAKPAY WGANS</div>
+
+      <div class="panel-body">
+
+      <form method="POST" action="">
+
+        <div class="form-group">
+
+          <label>REFF LO BOS:</label>
+
+          <input class="form-control" id="reff" placeholder="REFFLO" name="reff" required="" type="text" autocomplete="off">
+
+        </div>
+
+        <input class="btn btn-info" type="submit" name="submit">
+
+      </div>
+
+    </div>
+
+  </div>
